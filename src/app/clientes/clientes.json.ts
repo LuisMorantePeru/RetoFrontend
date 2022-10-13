503d710039f5384ca16387a7730e4ddeb683cf0b
LuisMorante <luis.morante1994@gmail.com>
2022-10-13 15:16:04 -0500

    Reto Frontend

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+// import { Cliente} from './cliente';
+
+// export const CLIENTES: Cliente[]=[
+//     {id:1, nombre:'Luis', apellido:'Morante', email:'luis@gmail.com', createAt:'2022-10-13'},
+//     {id:2, nombre:'Jesus', apellido:'Alejos', email:'jesus@gmail.com', createAt:'2022-10-13'},
+//     {id:3, nombre:'Rosa', apellido:'Carbajal', email:'rosa@gmail.com', createAt:'2022-10-13'},
+//     {id:4, nombre:'Fernando', apellido:'Monzon', email:'fernando@gmail.com', createAt:'2022-10-13'},
+//     {id:5, nombre:'Ana', apellido:'Rosales', email:'ana@gmail.com', createAt:'2022-10-13'},
+//     {id:6, nombre:'Humberto', apellido:'Sanchez', email:'humberto@gmail.com', createAt:'2022-10-13'},
+//     {id:7, nombre:'Christian', apellido:'Dominguez', email:'christian@gmail.com', createAt:'2022-10-13'},
+//     {id:8, nombre:'Esther', apellido:'Santander', email:'esther@gmail.com', createAt:'2022-10-13'},
+//     {id:9, nombre:'Omar', apellido:'Perez', email:'omar@gmail.com', createAt:'2022-10-13'},
+//     {id:10, nombre:'Lucia', apellido:'Alayo', email:'lucia@gmail.com', createAt:'2022-10-13'},
+//     {id:11, nombre:'Hambar', apellido:'Guevara', email:'hambar@gmail.com', createAt:'2022-10-13'},
+// ];
